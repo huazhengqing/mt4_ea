d64c05f008cb6139c0004e5acdeffb61a4afc569
huazhengqing <36689326+huazhengqing@users.noreply.github.com>
2018-06-20 12:27:18 +0800

    mm

--- copy.bat
+++ copy.bat
@@ -3,12 +3,10 @@ cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal"
 copy  "*.bat"    "D:\dev\mm\mt4_ea\"
 
 
-cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\39369E9A010897DDC6DFB2F026E04236\"
+cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\24DDD2AF4B390B873FBAE45354FC1591\"
 #xcopy ".\profiles\A_wq_bitcoin\*"    		"D:\dev\mm\mt4_ea\profiles\A_wq_bitcoin\"      /s /E /i /y
 #xcopy ".\profiles\A_wq_usd\*"    			"D:\dev\mm\mt4_ea\profiles\A_wq_usd\"      /s /E /i /y
 #xcopy ".\profiles\A_wq_commodity\*"    			"D:\dev\mm\mt4_ea\profiles\A_wq_commodity\"      /s /E /i /y
-
-
 
 copy ".\templates\A_*"    					"D:\dev\mm\mt4_ea\templates\"
 copy ".\MQL4\Experts\A_*.mq4"    			"D:\dev\mm\mt4_ea\MQL4\Experts\"
@@ -16,27 +14,13 @@ copy ".\MQL4\Include\wq_*"    				"D:\dev\mm\mt4_ea\MQL4\Include\"
 copy ".\MQL4\Indicators\A_*.mq4"   			"D:\dev\mm\mt4_ea\MQL4\Indicators\"
 copy ".\MQL4\Indicators\Sonic*.mq4"   		"D:\dev\mm\mt4_ea\MQL4\Indicators\"
 
-
-
 xcopy "D:\dev\mm\mt4_ea\*"     "D:\百度云同步盘\stock\MQL4_MQL5\wangqing\mt4_ea\"  /s /E /i /y
 
-
-cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\68F860AB0483135E3D2C8B03DCB3B032\"
+cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\C142B020C05FAD9EEC4BE1375F709241\"
 xcopy "D:\dev\mm\mt4_ea\*"     "."   /s /E /i /y
-
 
 cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\C348917D9E28C59E863914247686464D\"
 xcopy "D:\dev\mm\mt4_ea\*"     "."   /s /E /i /y
-
-
-cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\CC6AAA3367A6592D7E05F97A0A1C9C96\"
-xcopy "D:\dev\mm\mt4_ea\*"     "."   /s /E /i /y
-
-
-cd "%UserProfile%\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\"
-xcopy "D:\dev\mm\mt4_ea\*"     "."   /s /E /i /y
-
-
 
 
 #pause
